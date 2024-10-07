8889b500a555681d0c23140a3874398f6ae2e180
Balaganesh S <balaganesh@juspay.in>
2024-10-07 12:02:16 +0530

    Added a dummy project

---
+++ JenkinsTest/JenkinsTest/JenkinsTest.h
@@ -0,0 +1,16 @@
+//
+//  JenkinsTest.h
+//  JenkinsTest
+//
+//  Created by Balaganesh Balaganesh on 07/10/24.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JenkinsTest: NSObject
+
+- (void)testFunction;
+
+@end
+
+
---
+++ JenkinsTest/JenkinsTest/JenkinsTest.m
@@ -0,0 +1,17 @@
+//
+//  JenkinsTest.m
+//  JenkinsTest
+//
+//  Created by Balaganesh Balaganesh on 07/10/24.
+//
+
+#import <Foundation/Foundation.h>
+#import "JenkinsTest.h"
+
+@implementation JenkinsTest
+
+- (void)testFunction {
+    NSLog(@"Inside JenkinsTest test function");
+}
+
+@end
